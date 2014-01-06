e967b75603e0864c34dbf3d2546751b6134671ad
Kurt Lin <kurt@eda.ee.nctu.edu.tw>
2014-01-06 21:12:17 +0800

    Change to vector container

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
 bool found = false;
+vector< vector<int> > state;
 
-void print_state(int a[9]){
+void print_state(vector<int> a){
 	for (int i=0; i <= 8; i++){
 		if (i%3 == 2)
 			cout << a[i] << endl;
@@ -13,12 +16,38 @@ void print_state(int a[9]){
 	}
 }
 
-void check_state(int a[9], bool s[9]){
+bool check_path(vector<int> a){
+	bool exist = false;
+	vector < vector<int> >::iterator itr;
+	for (itr = state.begin(); itr != state.end(); ++itr){
+		if (*itr == a){
+			cout << "====Eq Array A====" << endl;
+			print_state(*itr);
+			cout << "====Eq Array B====" << endl;
+			print_state(a);
+			cout << "====End Array ====" << endl;
+			exist = true;
+		}
+	}
+	if (!exist)
+		state.push_back(a);
+	return exist;
+}
+
+bool completeness(vector<int> a){
+	bool comp = true;
+	int i;
+	for (i=0; i<=8; i++){
+		if (a[i] != (i+1)%9)
+			comp = false;
+	}
+	return comp;
+}
+
+void check_state(vector<int> a){
 	int i;
 	int p, q;
-	int m[9];
-	bool n[9];
-	int tmp;
+	vector<int> m;
 	if (!found){
 		for (i=0; i <= 8; i++){
 			if (a[i] == 0){
@@ -26,72 +55,59 @@ void check_state(int a[9], bool s[9]){
 				q = i%3;
 			}
 		}
-		if ((a[0] == 1) && (a[1] == 2) && (a[2] == 3) &&
-				(a[3] == 4) && (a[4] == 5) && (a[5] == 6) &&
-				(a[6] == 7) && (a[7] == 8) && (a[8] == 0)) {
+		if (completeness(a)){
 			print_state(a);
 			found = true;
 		}
 		else{
 			print_state(a);
 			cout << "----" << endl;
-			if (((p-1)>=0) && (s[(p-1)*3+q] == false)){
-				for (int x=0; x<=8; x++){
-					m[x] = a[x];
-					n[x] = s[x];
+			if ((p+1)<=2){ // down
+				m = a;
+		   	m[p*3+q] = m[(p+1)*3+q];
+		   	m[(p+1)*3+q] = 0;
+				if (!check_path(m)){
+					check_state(m);
 				}
+			}
+			if ((q+1)<=2){ // right
+				m = a;
+		   	m[p*3+q] = m[p*3+(q+1)];
+		   	m[p*3+(q+1)] = 0;
+				if (!check_path(m)){
+					check_state(m);
+				}
+			}
+			if ((p-1)>=0){ // up
+				m = a;
 				m[p*3+q] = m[(p-1)*3+q];
 				m[(p-1)*3+q] = 0;
-				n[(p-1)*3+q] = true;
-				check_state(m, n);
-			}
-			if (((p+1)<=2) && (s[(p+1)*3+q] == false)){
-				for (int x=0; x<=8; x++){
-					m[x] = a[x];
-					n[x] = s[x];
+				if (!check_path(m)){
+					check_state(m);
 				}
-				m[p*3+q] = m[(p+1)*3+q];
-				m[(p+1)*3+q] = 0;
-				n[(p+1)*3+q] = true;
-				check_state(m, n);
 			}
-			if (((q-1)>=0) && (s[p*3+(q-1)] == false)){
-				for (int x=0; x<=8; x++){
-					m[x] = a[x];
-					n[x] = s[x];
+			if ((q-1)>=0){ // left
+				m = a;
+		   	m[p*3+q] = m[p*3+(q-1)];
+		   	m[p*3+(q-1)] = 0;
+				if (!check_path(m)){
+					check_state(m);
 				}
-				m[p*3+q] = m[p*3+(q-1)];
-				m[p*3+(q-1)] = 0;
-				n[p*3+(q-1)] = true;
-				check_state(m, n);
-			}
-			if (((q+1)<=2) && (s[p*3+(q+1)] == false)){
-				for (int x=0; x<=8; x++){
-					m[x] = a[x];
-					n[x] = s[x];
-				}
-				m[p*3+q] = m[p*3+(q+1)];
-				m[p*3+(q+1)] = 0;
-				n[p*3+(q+1)] = true;
-				check_state(m, n);
 			}
 		}
 	}
 }
 
 int main (){
-	int a[9];
-	bool s[9];
+	vector<int> a;
 
 	for (int i=0; i <= 8; i++){
-		cin >> a[i];
-	  if (a[i] == 0)
-	  	s[i] = true;
-	  else
-	  	s[i] = false;
+		int t;
+		cin >> t;
+		a.push_back(t);
 	}
 
-	check_state(a, s);
+	check_state(a);
 
 	return 0;
 }
